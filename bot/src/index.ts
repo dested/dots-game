dc3b4a4ddb4a0680b8b9da8e4360e0923743a10d
Salvatore <dested@gmail.com>
2020-01-10 23:32:06 -0700

    rough bot psuedo

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -14,4 +14,28 @@ async function main() {
     await Utils.timeout(140);
   }
 }
+
+/*
+ * foreach emitters
+ *  if enemy swarm is 4 radius away
+ *    under attack=true
+ *  else
+ *    under attack=false
+ *
+ * if not under attack
+ *   if i have 2x the min to eat planet
+ *     send that percent off to eat planet
+ *   else
+ *     wait
+ * else
+ *   fortify around planet
+ *
+ * foreach non emitter swarm
+ *  if moving
+ *    check if enemy swarm is near
+ *      attack
+ *  else
+ *    if enemy hive is closer than new planet
+ *      do it
+ *  */
 main().catch(ex => console.error(ex));
